f8ca9130c89e581484eb430130c8b8c127485f18
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-09-10 19:29:06 +0200

    Drawing a circle to the screen

--- visualization/main.go
+++ visualization/main.go
@@ -4,9 +4,15 @@ import (
 	"errors"
 	"image/color"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
+
+func getMonitorCenter() (int, int) {
+	w, h := ebiten.Monitor().Size()
+	return w / 2, h / 2
+}
 
 type Game struct{}
 
@@ -21,17 +27,31 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{0xff, 0xff, 0xff, 0xff})
+	bounds := screen.Bounds().Size()
+	centerX := bounds.X / 2
+	centerY := bounds.Y / 2
+	drawCircle(screen, centerX, centerY, 20, color.White)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
+// Unfilled circle at (x, y)
+func drawCircle(screen *ebiten.Image, x, y int, radius int, clr color.Color) {
+	// Draw pixels completely around the x,y center point in a 360 degree = 2pi radians
+	for degrees := range(361) { //
+		radians := float64(degrees) * math.Pi / 180
+		ix := float64(radius) * math.Cos(radians)
+		iy := float64(radius) * math.Sin(radians)
+		screen.Set(x + int(math.Round(ix)), y + int(math.Round(iy)), clr)
+	}
+}
+
 func main() {
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	width, height := ebiten.Monitor().Size()
-	ebiten.SetWindowSize(width, height)
+	w, h := ebiten.Monitor().Size()
+	ebiten.SetWindowSize(w, h)
 	ebiten.SetWindowTitle("Physics Engine")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		if err.Error() != "" {
